7fd91ee5abb6d4a37e50d5b53495e9c1feba8df8
Marc Nopprapun <mnopprapun@gmail.com>
2021-03-19 15:56:22 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+
+const input = document.getElementById('input');
+
+function clickAlert() {
+  alert('I was clicked!');
+}
+
+input.addEventListener('click', clickAlert); 
+
 function addingEventListener() {
+    alert('I was clicked!');
+    input.addEventListener('click', addingEventListener);
 }
